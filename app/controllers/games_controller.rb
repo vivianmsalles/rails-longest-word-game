ae2b854fe2b1c069b091cc65974d67e6609d5e27
Vivian Salles <vivianmsalles@gmail.com>
2020-08-10 15:19:43 +0200

    challenge completed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,44 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    alphabet = ('A'..'Z').to_a
+    @letters = []
+    10.times { @letters.push(alphabet.sample) }
+  end
+
+  def score
+    word = params[:word].upcase
+    @letters = params[:letters]
+
+    if in_the_grid?(word, @letters) == false
+      @result = "Sorry but #{word} can't be build out of #{@letters}"
+    elsif english_word?(word) == false
+      @result = "Sorry but #{word} does not seem to be a valid English word..."
+    else
+      @result = "Congratulations! #{word} is a valid English word!"
+    end
+  end
+
+  private
+
+  def in_the_grid?(word, letters)
+    word_letters = word.split("")
+    word_array = []
+
+    word_letters.each { |character| word_array.push(character) if letters.include?(character) }
+
+    word.length == word_array.length
+  end
+
+  def parsing(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    answer_serialized = open(url).read
+    JSON.parse(answer_serialized)
+  end
+
+  def english_word?(word)
+    parsing(word)
+    parsing(word)['found']
+  end
+end
